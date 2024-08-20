fa6e0ea551480d640e23c2027d56bdd1aba8d5be
alecoder <alejandroccoder@proton.me>
2024-08-20 16:44:04 +0200

    Updated readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
 # Watch github user activity
+
+[Project Source](https://roadmap.sh/projects/github-user-activity)
+
+## Use
+```bash
+github-user-activity <username>
+```
+
+### Considerations
+- Shows the first 10 results only
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import fs from "fs"
-import { log } from 'console';
 
 const args = process.argv.splice(2);
 
 let url: string = `https://api.github.com/users/${args[0]}/events?page=${1}?per_page${10}`
 
+let path = "data/data.json"
+
 
 function checkCache() {
-  let data: any | null = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+  let data: any | null = JSON.parse(fs.readFileSync(path, 'utf-8'));
   for(let i = 0; i < data.length; i++) {
     if (data[i].actor.display_login == args[0]) {
       return data.filter((element: any) => element.actor.display_login == args[0])
@@ -35,12 +36,12 @@ async function fetchData() {
   let responseData = await fetch(url)
   dataFetch = await responseData.json();
 
-  let cacheData = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
+  let cacheData = JSON.parse(fs.readFileSync(path, 'utf-8'))
 
   for(let i = 0; i < dataFetch.length; i++) {
     cacheData.push(dataFetch[i])
   }
-  fs.writeFileSync('data.json', JSON.stringify(cacheData));
+  fs.writeFileSync(path, JSON.stringify(cacheData));
 }
 
 function displayData(data: any | null) {
